ae8c73597d9935e10687647b5a6e452fd289017f
drake bishop <drakemx85@gmail.com>
2023-01-30 23:36:59 -0600

    filled in code for three functions

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  if (someValue === 199) {
+    return 'This one is on me!'
+  } else if (someValue === 1500) {
+    return 'That will be twenty bucks.'
+  } else if (someValue === 2001) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (someValue === 2501) {
+    return 'No can do.'
+  }
+
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(1500);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+function ternaryCheckCity(someCity){
+  return someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+ternaryCheckCity(NYC);
+
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous' ) {
+    return 'Thank you so much.'
+  } else if (tip === 'not as generous') {
+    return 'Thank you.'
+  } else if (tip === 'thanks for everything') {
+    return 'Bye.'
+  }
 }
+
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip('thanks for everything');
